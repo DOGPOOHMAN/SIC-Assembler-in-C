2979e71fbeecdbf6c18048b7c0d40d8c2120a76b
Rick-Lee <li.dogpooh@gmail.com>
2016-04-24 00:57:35 +0800

    add setting custom Column wide of character of lable and opcode

--- sic_build_itmfile.c
+++ sic_build_itmfile.c
@@ -1,17 +1,23 @@
 #include "sic_build_itmfile_functions.c"
 #define DEBUG
 short pass1_built_itmfile(FILE * sicPgrm){
+	//store the character of read line
+	Line lines;
+	
 	//sic program format
-	short lableWide   =  8;
-	short opcodeWide  =  8;
-	short oprentWide  =  8;
+	lines.labWide   =  8;//column wide of lable
+	lines.codWide   =  8;//column wide of opcode
+
 	
-	//starting reading
-	char pgrmName[20];
-	char temp[10];
-	
+	//program infor
+	char     pgrmName [20];
 	unsigned startLoc =  0;
+	
+	
+	//will change in processing
+	char temp[10];
 	unsigned nowLoc   =  0;
+
 
 	short errorFlag   =  FALSE;
 	short stopReadFlag=  FALSE;
@@ -22,49 +28,54 @@ short pass1_built_itmfile(FILE * sicPgrm){
 	
 	
 	/* user setting program format */
-	pass1_set_pgrmformat(&lableWide, &opcodeWide, &oprentWide);
-//	
-	/* starting reading program*/
-	//read program name
-	fscanf(sicPgrm, "%s", pgrmName);
-	
-	fscanf(sicPgrm, "%s", temp);
-	//check Pseudo Instrution is "START"
-	if( strcmp(temp, PITABLE[0]) == 0){
-		
-		//read locctr and it maxima is 0x7888
-		fscanf(sicPgrm, "%x", &nowLoc);
-		if(nowLoc > 0x7888){
-			errorFlag = TRUE;
-		}
-		
-	}else{
-		errorFlag = TRUE;
-	}
-	
-	
+	pass1_set_pgrmformat(&lines);
+
 	#ifdef DEBUG
-	puts(pgrmName);
-	printf("locctr %#x\n", nowLoc);
+	printf("labWide %u\n", lines.labWide);
+	printf("codWide %u\n", lines.codWide);
 	#endif
-	
-	//out put error information
-	if(errorFlag == TRUE){
-	
-		puts("The program had wrong starting location or another problem!");
-		system("pause");
-		exit(1);
-	}else{
-		
-		errorFlag = FALSE;
-		startLoc  = nowLoc;
-	}
-	
-	
-	/* read program content */
-	while(stopReadFlag == FALSE){
-		stopReadFlag = TRUE;
-	}//end of while
+
+//	/* starting reading program*/
+//	//read program name
+//	fscanf(sicPgrm, "%s", pgrmName);
+//	
+//	fscanf(sicPgrm, "%s", temp);
+//	//check Pseudo Instrution is "START"
+//	if( strcmp(temp, PITABLE[0]) == 0){
+//		
+//		//read locctr and it maxima is 0x7888
+//		fscanf(sicPgrm, "%x", &nowLoc);
+//		if(nowLoc > 0x7888){
+//			errorFlag = TRUE;
+//		}
+//		
+//	}else{
+//		errorFlag = TRUE;
+//	}
+//	
+//	
+//	#ifdef DEBUG
+//	puts(pgrmName);
+//	printf("locctr %#x\n", nowLoc);
+//	#endif
+//	
+//	//out put error information
+//	if(errorFlag == TRUE){
+//	
+//		puts("The program had wrong starting location or another problem!");
+//		system("pause");
+//		exit(1);
+//	}else{
+//		
+//		errorFlag = FALSE;
+//		startLoc  = nowLoc;
+//	}
+//	
+//	
+//	/* read program content */
+//	while(stopReadFlag == FALSE){
+//		stopReadFlag = TRUE;
+//	}//end of while
 	
 	return errorFlag;
 }//end of pass1_built_itmfile function
--- sic_build_itmfile_functions.c
+++ sic_build_itmfile_functions.c
@@ -1,2 +1,81 @@
-void pass1_set_pgrmformat(int *lableWide, int *opcodeWide,int *oprentWide);
+typedef struct line{
+	//sic program format
+	unsigned labWide;//column wide of lable
+	unsigned codWide;//column wide of opcode
+	
+	char all   [60];
+	char lable [16];
+	char code  [16];
+	char oprent[16];
+} Line;
+
+
+void pass1_set_pgrmformat(Line * l){
+	char choice;
+	
+	printf("------------------------------------\n");
+	printf("Column wide of character of lable  :%d\n", l->labWide);
+	printf("Column wide of character of opcode :%d\n", l->codWide);
+	printf("SIC> Do you want to use default setting? (y/n):");
+	scanf(" %c", &choice);
+	
+	if(choice != 'y'){
+		
+		short setSuccess = FALSE;
+		unsigned tempLabWide;
+		unsigned tempCodWide;
+		
+		
+		
+		while(setSuccess == FALSE){
+			
+			//init choice
+			choice = ' ';
+		
+			printf("------------------------------------\n");
+			printf("lable  :");
+			scanf(" %u", &tempLabWide);
+			
+			printf("opcode :");
+			scanf(" %u", &tempCodWide);
+			
+			printf("SIC> Do you want to use this setting? (y/n):");
+			scanf(" %c", &choice);
+			
+			if(choice == 'y')
+			{
+				if( (tempLabWide <= 15) && (tempCodWide <= 15) ){
+					if( (tempLabWide >= 8) && (tempCodWide >= 8) ){
+						
+						setSuccess = TRUE;
+						l->labWide = tempLabWide;
+						l->codWide = tempCodWide;
+					}
+					else{
+						puts("Maximum wide of Lable and Opcode is [15]");
+						puts("Minimum wide of Lable and Opcode is [08]");
+					}
+				}
+				else{
+					puts("Maximum wide of Lable and Opcode is [15]");
+					puts("Minimum wide of Lable and Opcode is [08]");
+				}
+			} 
+		}//end of while
+	}//end of if
+
+}//end of pass1_set_pgrmformat function
+
+
+
+void pass1_init_line(Line * l){
+	strcpy(l->all,    "");
+	strcpy(l->lable,  "");
+	strcpy(l->code,   "");
+	strcpy(l->oprent, "");
+}
+
+//short pass1_is_comment()
+//
+//short pass1_pitable_start()
 
