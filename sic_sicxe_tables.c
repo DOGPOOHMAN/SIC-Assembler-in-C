6d126c8f2fe640b7cee098ca27b6fe22bbf161a1
Rick-Lee <li.dogpooh@gmail.com>
2016-05-16 00:04:25 +0800

    add a function(pass1_find_in_table(char * temp)) in sic_sicxe_tables.c

--- sic_sicxe_tables.c
+++ sic_sicxe_tables.c
@@ -1,38 +1,67 @@
 
+/* used to find code in opTable
+if this code can not be found in Table
+set "subscript" value be 99
+*/
 unsigned pass1_find_opcode(char * temp){
-	unsigned count = 0;
+	unsigned subscript = 0;
 	
-	for(; count < OPTABLE_LEN; count++){
-		if(strcmp(temp, OPTABLE[ count ].name) == 0){
+	for(; subscript < OPTABLE_LEN; subscript++){
+		if(strcmp(temp, OPTABLE[ subscript ].name) == 0){
 			break;
 		}
 	}
 	
 	//if not found
-	if(count >= OPTABLE_LEN){
-		count = 99;
+	if(subscript >= OPTABLE_LEN){
+		subscript = 99;
 	}
 	
-	return count;
+	return subscript;
 }//end of pass1_find_opcode function
 
 
+
+/* used to find code in piTable(pseudoTable)
+if this code can not be found in Table
+set "subscipt" value be 99
+*/
 unsigned pass1_find_pi(char * temp){
-	unsigned count = 0;
+	unsigned subscript = 0;
 	
-	for(; count < PSEUDO_INS_LEN; count++){
-		if(strcmp(temp, PITABLE[ count ]) == 0){
+	for(; subscript < PSEUDO_INS_LEN; subscript++){
+		if(strcmp(temp, PITABLE[ subscript ]) == 0){
 			break;
 		}
 	}
 	
 	//if not found
-	if(count >= PSEUDO_INS_LEN){
-		count = 99;
+	if(subscript >= PSEUDO_INS_LEN){
+		subscript = 99;
 	}
 	
-	return count;
+	return subscript;
 }//end of pass1_find_opcode function
+
+
+
+/* used to find code in opTable and piTable 
+if this code can not be found in Tables
+set "subscript" value be 99
+*/
+unsigned pass1_find_in_table(char * temp){
+	unsigned subscript = 0;
+	
+	//find in opTable
+	subscript = pass1_find_opcode(temp);
+	
+	//find in pseudoTable
+	if(subscript == 99){
+	    subscript = pass1_find_pi(temp);
+    }
+	
+	return subscript;
+}//end of pass1_find_in_table function
 
 //SYMTABLE * pass1_init_symtable(char * lable, unsigned locctr){
 //	SYMTABLE * temp;
